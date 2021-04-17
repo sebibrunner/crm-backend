caf13f7b4e061bd54aac2da1f4367f3e3631ccd7
Sebastian Brunner <sebastianbrunner@hotmail.ch>
2021-04-17 17:13:55 +0200

    Add typeorm stuff for address

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,1 +1,16 @@
-export class Address {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Address {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  zip: number;
+
+  @Column()
+  street: string;
+
+  @Column()
+  houseNumber: number;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,20 @@ import { InvoiceModule } from './invoice/invoice.module';
 import { CustomerModule } from './customer/customer.module';
 import { UserModule } from './user/user.module';
 import { AddressModule } from './address/address.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'crm_db_user',
+      password: 'changeme',
+      database: 'crm_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
     ProductModule,
     PositionModule,
     DocumentModule,
