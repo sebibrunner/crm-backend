1b877d48bbbf4b52d587447dd86414eb49a54694
Sebastian Brunner <sebastianbrunner@hotmail.ch>
2021-08-28 23:50:25 +0200

    switch to db-uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,11 +25,7 @@ const ENV = process.env.NODE_ENV;
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10) || 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
