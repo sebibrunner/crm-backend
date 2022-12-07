52701033677128e2de21ab0e810e27e89199229a
Sebastian Brunner <sebastianbrunner@hotmail.ch>
2022-12-07 19:35:14 +0100

    just fixing stuff

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true});
   const PORT = process.env.PORT || 5000;
   await app.listen(PORT);
 }
